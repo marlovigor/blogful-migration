3827b35956d44cc60549c170ac6f4bff1e47668d
marlo <apple@marlo-MacBook-Pro.local>
2020-06-09 17:08:24 -0400

    3rd commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const ArticlesService = require('./articles-service')
+const articlesRouter = require('./articles/articles-router')
+
 
 const app = express()
 
@@ -15,33 +16,13 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+
+app.use('/articles', articlesRouter)
+
+
 app.get('/', (req, res) => {
-
-    res.send('Hello, boilerplate!')
-
+    res.send('Hello, world!')
 })
 
-app.get('/articles', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getAllArticles(knexInstance)
-        .then(articles => {
-            res.json(articles)
-        })
-        .catch(next)
-})
-
-app.get('/articles/:article_id', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getById(knexInstance, req.params.article_id)
-        .then(article => {
-            if (!article) {
-                return res.status(404).json({
-                    error: { message: `Article doesn't exist` }
-                })
-            }
-            res.json(article)
-        })
-        .catch(next)
-})
 
 module.exports = app
