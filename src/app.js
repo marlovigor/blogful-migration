778d126775d7877f8be80b9b4ebfc0358721987b
marlo <apple@marlo-MacBook-Pro.local>
2020-06-09 12:03:55 -0400

    2nd commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -18,6 +19,29 @@ app.get('/', (req, res) => {
 
     res.send('Hello, boilerplate!')
 
+})
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            if (!article) {
+                return res.status(404).json({
+                    error: { message: `Article doesn't exist` }
+                })
+            }
+            res.json(article)
+        })
+        .catch(next)
 })
 
 module.exports = app
--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-  }
+    "connectionString": (process.env.NODE_ENV === 'test')
+     ? process.env.TEST_DB_URL
+     : process.env.DB_URL,
+    }
